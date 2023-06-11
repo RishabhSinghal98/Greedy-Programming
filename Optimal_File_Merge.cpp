6020ca886bf4c527f09cfa18ab89b0a2f7afa0f3
Rishabh Singhal <123990866+RishabhSinghal98@users.noreply.github.com>
2023-06-11 23:56:39 +0530

    Add files via upload

---
+++ Optimal_File_Merge.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int mincost(vector<int> &files,int size)
+{
+	priority_queue<int,vector<int>,greater<int>> pq;
+	int total_min_cost=0;
+	
+	for(int i=0;i<size;i++)
+	{
+		pq.push(files[i]);
+	}
+	int temp_cost=0;
+	int count=0;
+	
+	while(pq.size()>1)
+	{
+	    int temp_cost=0;
+		temp_cost+=pq.top();
+		pq.pop();
+		temp_cost+=pq.top();
+		pq.pop();
+		total_min_cost+=temp_cost;
+		pq.push(temp_cost);
+	}
+	return total_min_cost;
+}
+
+int main()
+{
+	int size;
+	cout<<"Enter Size of array"<<endl;
+	cin>>size;
+	int n;
+	vector<int> files;
+	cout<<"Enter sizes  of files"<<endl;
+	for(int i=0;i<size;i++)
+	{
+		cin>>n;
+		files.push_back(n);
+	}
+
+    int cost=mincost(files,size);
+    
+    cout<<"Minimumum Computations Are "<<cost;
+	
+	return 0;
+	
+	
+	
+	
+}
